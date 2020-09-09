1c02d3f50088e0f1708cec6672adbbf1db53059d
André Luiz Vieira <albvs@cin.ufpe.br>
2020-09-08 21:13:00 -0300

    Update neighbour_parallel.cu

--- VoxarMPS/VoxarMPS/neighbour_parallel.cu
+++ VoxarMPS/VoxarMPS/neighbour_parallel.cu
@@ -1,5 +1,6 @@
 /*
 Copyright (C) 2018-2020 Deep Tavker (tavkerdeep@gmail.com)
+Copyright (C) 2020 André Luiz Vieira-e-Silva (albvs@cin.ufpe.br)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
